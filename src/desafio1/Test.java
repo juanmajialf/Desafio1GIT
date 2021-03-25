9318dab85e9ad36488a3dce4c2d5c8206cea5bee
juanmajialf <juanma.jimenez.alfaro@gmail.com>
2021-03-25 13:04:41 +0100

    Version 1.0

---
+++ src/desafio1/Test.java
@@ -0,0 +1,18 @@
+
+package desafio1;
+
+/**
+ * @author jjimealf
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hola mundo");
+	}
+
+}
